0485f712957fbaefa40b6834c090cd9cf7c51220
Demetrios Alves <demetrios.10@hotmail.com>
2026-06-07 13:34:53 -0300

    update

--- tests/test_locators.py
+++ tests/test_locators.py
@@ -4,6 +4,7 @@ def test_get_by_role(page):
     page.get_by_role("link", name="Signup / Login").click()
     page.get_by_role("button", name="Login").click()
     
-    
+
+# isso é um teste de exemplo para demonstrar o uso do método get_by_role do Playwright. Ele navega até a página de login e tenta clicar no botão de login sem preencher os campos de email e senha, o que deve resultar em uma mensagem de erro.
     
     
