ea10513935d9b8c276810f699602dfceb7f6dc92
Demetrios Alves <demetrios.10@hotmail.com>
2026-06-11 01:03:50 -0300

    up

--- tests/test_locators.py
+++ tests/test_locators.py
@@ -1,13 +1,19 @@
 
 def test_get_by_role(page):
-    page.goto("https://automationexercise.com/") # Acessar a página inicial
-    page.get_by_role("link", name="Signup / Login").click() # Acessar a página de login
+    # Acessar a página inicial
+    page.goto("https://automationexercise.com/")
+    # Acessar a página de login
+    page.get_by_role("link", name="Signup / Login").click() 
+    # Clicar no botão de login sem preencher os campos para verificar a mensagem de erro
     page.get_by_role("button", name="Login").click()
-    page.get_by_role("button", name="Signup").click()
+    # Clicar no botão de cadastro sem preencher os campos para verificar a mensagem de erro
+    page.get_by_role("button", name="Signup").click() 
+
     
-    # o comando get_by_role é utilizado para localizar elementos com base em seu papel (role) e nome (name).
-    # No exemplo acima, o código acessa a página inicial do site "https://automationexercise.com/",
-    # clica no link "Signup / Login" para acessar a página de login, em seguida, clica nos botões "Login" e "Signup".
-    # O comando get_by_role é útil para localizar elementos de forma mais semântica, 
-    # permitindo que os testes sejam mais legíveis e menos propensos a 
-    # falhas devido a mudanças na estrutura do DOM.
+def test_get_by_text(page):
+    # Acessar a página inicial e clicar no link "Home" para verificar se o link está funcionando corretamente
+    page.goto("https://automationexercise.com/") 
+    # Adicionar uma pausa para permitir que a página carregue completamente antes de interagir com os elementos
+    page.pause()
+    # Acessar a página inicial e clicar no link "Website for automation practice" para verificar se o link está funcionando corretamente
+    page.get_by_text("Full-Fledged practice website for").click()
